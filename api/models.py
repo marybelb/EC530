682b6f4508774eb00e96f65ea42a95cbe5ba9591
Marybel Boujaoude <91172956+marybelb@users.noreply.github.com>
2024-03-24 20:46:16 -0400

    Create models.py
    users, roles, and devices

---
+++ api/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
+
+user_roles = Table('user_roles', Base.metadata,
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    Column('role_id', ForeignKey('roles.id'), primary_key=True)
+)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    roles = relationship('Role', secondary=user_roles, back_populates='users')
+
+class Role(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    users = relationship('User', secondary=user_roles, back_populates='roles')
+
+class Device(Base):
+    __tablename__ = 'devices'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50), nullable=False)
+    is_enabled = Column(Boolean, default=True)
