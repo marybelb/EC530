52f10c47132b9a2a626a302d678c6c0e8c949a0f
Marybel Boujaoude <91172956+marybelb@users.noreply.github.com>
2024-03-24 21:51:43 -0400

    Create main.js

---
+++ app/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Custom script to enhance forms, handle AJAX requests, etc.
+    console.log('The document is ready!');
+});
+
+// More complex functionality as your application develops
